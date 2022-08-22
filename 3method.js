e7bcca86d12e7c988ef72a1049e0ea0da93fe66b
Ali Gharehkhani <101476424+AliGharehkhani@users.noreply.github.com>
2022-08-22 14:33:31 +0430

    working with some examples about bind() method in javascript through the w3schools

--- 3method.js
+++ 3method.js
@@ -10,10 +10,46 @@ const person = {
 
 function registerUser() {
   //Aditional functionallity....
-  this.getName()
+  this.getName();
 }
 
 // and we can binding the person object to registerUser function in this way:
 const register = registerUser.bind(person);
 
 register();
+
+//w3schools
+//With the bind() method, an object can borrow a method from another object.
+//examples:👇👇👇
+
+const odd = {
+  firstName: "John",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+const member = {
+  firstName: "Hege",
+  lastName: "Nilsen",
+};
+
+let fulName = odd.fullName.bind(member);
+
+console.log(fulName());
+
+//in the following example, the person object has a display method. in the display 
+//method, this refers to the person object:
+
+const Person = {
+  firstName:"John",
+  lastName: "Doe",
+  display: function() {
+    let x = document.getElementById("demo");
+    x.innerHTML = this.firstName + " " + this.lastName;
+  }
+}
+
+Person.display();
+
