9c31986dc7162a759f958f967e2166e0ce6a6517
Ali Gharehkhani <101476424+AliGharehkhani@users.noreply.github.com>
2022-08-31 19:15:02 +0430

    prototype chapter is stoped

--- prototype.js
+++ prototype.js
@@ -92,3 +92,39 @@ const todo2 = new Todo("Do my homework", false);
 todo.getTodoName();
 
 console.log(todo , todo2);
+
+// prototypal inheritence
+
+function Enemy(life, name , level) {
+  this.life = life;
+  this.name = name;
+  this.level = level;
+}
+
+Enemy.prototype.getInfo = function() {
+  console.log(this.life, this.name, this.level)
+};
+
+Enemy.prototype.attack = function() {
+  console.log(`${this.name} has attacked!`)
+};
+
+Enemy.prototype.block = function() {
+  console.log(`${this.name} has blocked`)
+};
+
+function Dragon(life , name , level, color, spell){
+  Enemy.call(this, life, name , level);
+  //Define it as normal
+  this.color = color;
+  this.spell = spell;
+}
+
+Dragon.prototype.fireBreath = function(){
+  console.log("Fire BREATHHH!");
+}
+
+const newDragon = new Dragon(100 , "Drogon", 25 , "green", "fire");
+
+
+console.log(newDragon)
