41a2316ae04c712a113cbfb33414191bc46aa801
Ali Gharehkhani <101476424+AliGharehkhani@users.noreply.github.com>
2022-09-01 17:28:47 +0430

    part 5 of this tutorial ended with some fun stuff with oop

--- classes.js
+++ classes.js
@@ -1,40 +1,78 @@
-function Human(alive, name, level) {
-  this.life = alive;
-  this.name = name;
-  this.level = level;
-}
+// function Human(alive, name, level) {
+//   this.life = alive;
+//   this.name = name;
+//   this.level = level;
+// }
 
-const number1 = new Human(true, "ali", "intermidiate");
-console.log(number1);
+// const number1 = new Human(true, "ali", "intermidiate");
+// console.log(number1);
 
-//we can make above code beautiful by using classes:
+// //we can make above code beautiful by using classes:
 
-class Enemy {
-  constructor(life, name, level) {
-    this.life = life;
-    this.name = name;
-    this.level = level;
+// class Enemy {
+//   constructor(life, name, level) {
+//     this.life = life;
+//     this.name = name;
+//     this.level = level;
+//   }
+//   getInfo() {
+//     console.log(this.life, this.name, this.level);
+//   }
+// }
+
+// const turtle = new Enemy(25, "Squidlebuggl", 10);
+// const bug = new Enemy(1, "bugymugy", 1);
+// turtle.getInfo();
+// bug.getInfo();
+
+// class Bug extends Enemy {
+//     constructor(life, name, level, legs,dmg) {
+//         super(life, name , level);
+//         //
+//         this.legs = legs;
+//         this.dmg = dmg;
+//     }
+// }
+
+// const john = new Bug(10 , "Buggy" , 1 , 6 , 10);
+
+// console.log(john);
+// john.getInfo();
+
+//Fun Stuff with OOP:
+
+class Animator {
+  constructor(selector) {
+    this.selector = document.querySelector(selector);
   }
-  getInfo() {
-    console.log(this.life, this.name, this.level);
-  }
-}
-
-const turtle = new Enemy(25, "Squidlebuggl", 10);
-const bug = new Enemy(1, "bugymugy", 1);
-turtle.getInfo();
-bug.getInfo();
-
-class Bug extends Enemy {
-    constructor(life, name, level, legs,dmg) {
-        super(life, name , level);
-        //
-        this.legs = legs;
-        this.dmg = dmg;
+  fadeOut(time, toggle = false) {
+    if (toggle && this.selector.classList.contains("fadeOut-active")) {
+      this.selector.style.opacity = 1;
+      this.selector.classList.remove("fadeOut-active");
+    } else {
+      this.selector.style.transition = `all ${time}s ease`;
+      this.selector.style.opacity = 0;
+      this.selector.classList.add("fadeOut-active");
     }
+  }
+  move(time, toggle = false, { x = 0, y = 0 }) {
+    if (toggle && this.selector.classList.contains("move-active")) {
+      this.selector.style.transform = "translate(0px, 0px";
+      this.selector.classList.remove("move-active");
+    } else {
+      this.selector.style.transition = `all ${time}s ease`;
+      this.selector.style.transform = `translate(${x}px,${y}px)`;
+      this.selector.classList.add("move-active");
+    }
+  }
 }
 
-const john = new Bug(10 , "Buggy" , 1 , 6 , 10);
+const intro = new Animator("h1");
+const btnAnim = new Animator("button");
+const button = document.querySelector("button");
 
-console.log(john);
-john.getInfo();
+button.addEventListener("click", () => {
+  intro.move(1, true, { x: 100, y: 200 });
+  intro.fadeOut(2 , true)
+  btnAnim.move(2, true, { x: 500, y: 30 });
+});
